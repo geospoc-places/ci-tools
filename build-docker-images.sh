f7843264306360a019f45ffc5a1cb68bfcc20ef1
Ajinkya Patil <140216+ajinkya@users.noreply.github.com>
2022-06-20 23:29:51 +0530

    feat(ci): added support for passing npm  token for GHCR

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -52,8 +52,13 @@ fi
 # log in to Docker Hub or GHCR _before_ building to avoid rate limits
 docker login $DOCKER_REGISTRY -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
+ARGS=""
+if [[ -z "$NODE_AUTH_TOKEN" ]]; then
+   ARGS="--build-arg NODE_AUTH_TOKEN=$NODE_AUTH_TOKEN"
+fi
+
 # Build and push each tag (the built image will be reused after the first build)
 for tag in ${tags[@]}; do
-  docker build -t $tag .
+  docker build -t $tag $ARGS . 
   docker push $tag
 done
